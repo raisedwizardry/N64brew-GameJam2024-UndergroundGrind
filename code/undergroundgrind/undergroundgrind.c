3d1764e3a1f0d1867970cdd863512dbbd0396a77
raisedwizardry <fuzzy.gift9058@frozenincode.com>
2025-01-03 13:50:10 -0500

    remove music

--- code/undergroundgrind/undergroundgrind.c
+++ code/undergroundgrind/undergroundgrind.c
@@ -59,7 +59,6 @@ T3DModel *chestModel;
 T3DVec3 camPos;
 T3DVec3 camTarget;
 T3DVec3 lightDirVec;
-xm64player_t music;
 
 int chestBlockNumber;
 
@@ -192,8 +191,6 @@ void minigame_init(void)
   wav64_open(&sfx_countdown, "rom:/core/Countdown.wav64");
   wav64_open(&sfx_stop, "rom:/core/Stop.wav64");
   wav64_open(&sfx_winner, "rom:/core/Winner.wav64");
-  xm64player_open(&music, "rom:/undergroundgrind/bottled_bubbles.xm64");
-  xm64player_play(&music, 0);
 }
 
 void player_do_damage(SnakePlayer *player)
@@ -548,8 +545,6 @@ void minigame_cleanup(void)
   wav64_close(&sfx_countdown);
   wav64_close(&sfx_stop);
   wav64_close(&sfx_winner);
-  xm64player_stop(&music);
-  xm64player_close(&music);
   rspq_block_free(dplMap);
 
   t3d_model_free(snakeModel);
--- code/undergroundgrind/undergroundgrind.mk
+++ code/undergroundgrind/undergroundgrind.mk
@@ -8,7 +8,6 @@ ASSETS_LIST += \
 	filesystem/undergroundgrind/shadow.i8.sprite \
     filesystem/undergroundgrind/sand12.ci4.sprite \
     filesystem/undergroundgrind/stone.ci4.sprite \
-    filesystem/undergroundgrind/bottled_bubbles.xm64 \
 	filesystem/undergroundgrind/m6x11plus.font64
 
 filesystem/undergroundgrind/m6x11plus.font64: MKFONT_FLAGS += --outline 1 --size 36
